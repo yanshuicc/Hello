2564cef815d9b7edd9822a99fe76af45a4d26d34
yanshuicc <yanshuichongcha@outlook.com>
2015-07-08 22:07:43 +0800

    add and commit first time

---
+++ Main/Hello.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+{
+	printf("Hello, this is my first time of using git-hub.\n");
+	printf("After a noon, I think I could use it preliminarily.\n");
+	printf("There is no doubt it is a interesting, great tool, I like it.\n");
+	printf("I would use it since now.\n");
+	printf("I will try its features with this demo.\n");
+	return 0;
+}
